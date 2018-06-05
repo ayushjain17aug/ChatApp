3172b53f94b6561a8c68cb20be2be34b15a382ff
Ayush Jain <ayushjain17aug@gmail.com>
2018-06-05 17:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 #!/bin/env python
 import sys
-from chat import create_app, socketio
+from flask import Flask
+from flask_socketio import SocketIO
+from flask import Blueprint
+
+
+socketio = SocketIO()
+main_blueprint = Blueprint('main', __name__)
+
+def create_app(debug=False):
+    """Create an application."""
+    app = Flask(__name__)
+    app.debug = debug
+    app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+    app.register_blueprint(main_blueprint)
+
+    socketio.init_app(app)
+    return app
 
 app = create_app(debug=True)
 
