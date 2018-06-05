91b1d53461401de9e22117d82c117a4577bd739f
Ayush Jain <ayushjain17aug@gmail.com>
2018-06-05 18:41:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-#!/bin/env python
-from chat.create import create_app, socketio
+from flask import Flask 
+from flask_socketio import SocketIO, send
 
-app = create_app(debug=True)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+	socketio.run(app)
