45dfbc786b0ab96a6778719a8e1fa2b5bde4709a
Ayush Jain <ayushjain17aug@gmail.com>
2018-06-05 17:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from flask import Blueprint
 
 
 socketio = SocketIO()
-main_blueprint = Blueprint('main', __name__)
 
 def create_app(debug=False):
     """Create an application."""
     app = Flask(__name__)
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     socketio.init_app(app)
